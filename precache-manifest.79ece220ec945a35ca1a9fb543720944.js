d2702c7f294aa55ce17eab6ddcdd9766a154cbc0
shivam rawat <92804859+Shivamraj6370@users.noreply.github.com>
2023-09-28 23:36:33 +0530

    Updates

---
+++ precache-manifest.79ece220ec945a35ca1a9fb543720944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0eb1489506f4eca1448",
+    "url": "/BlackFox/static/css/main.0ee4831f.chunk.css"
+  },
+  {
+    "revision": "c0eb1489506f4eca1448",
+    "url": "/BlackFox/static/js/main.c0eb1489.chunk.js"
+  },
+  {
+    "revision": "2e76ef4f4b07ab805959",
+    "url": "/BlackFox/static/js/1.2e76ef4f.chunk.js"
+  },
+  {
+    "revision": "990e6f574171999b44e8",
+    "url": "/BlackFox/static/js/2.990e6f57.chunk.js"
+  },
+  {
+    "revision": "253cb85d982e5dfaf689",
+    "url": "/BlackFox/static/js/runtime~main.253cb85d.js"
+  },
+  {
+    "revision": "e59594906d140cd5aec13ecbbad5c69e",
+    "url": "/BlackFox/index.html"
+  }
+];
